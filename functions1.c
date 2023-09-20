ef0bfa73d7e7269f4b154d65ddce982535036ca5
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-09-20 18:54:30 +0000

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,171 +1,233 @@
 #include "main.h"
 
-/*** PRINT UNSIGNED NUMBER ***/
-
 /**
- * print_unsigned - Prints an unsigned number
- * @types: arguments list
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of char
+ * print_pointer - prints an hexgecimal number
+ * @val: arguments
+ * Return: counter
  */
-
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_pointer(va_list val)
 {
-	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
 
-	num = convert_size_unsgnd(num, size);
-
-	if (num == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-
-	while (num > 0)
+	p = va_arg(val, void*);
+	if (p == NULL)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
 	}
 
-	i++;
-
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = print_hex_aux(a);
+	return (b + 2);
 }
 
-/*** PRINT UNSIGNED NUMBER IN OCTAL ***/
+#include "main.h"
 
 /**
- * print_octal - Prints an unsigned number in octal notation
- * @types: arguments list
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of char printed
+ * print_oct - prints an octal number
+ * @val: arguments
+ * Return: counter
  */
-
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_oct(va_list val)
 {
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
 
-	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
-
-	UNUSED(width);
-
-	num = convert_size_unsgnd(num, size);
-
-	if (num == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-
-	while (num > 0)
+	while (num / 8 != 0)
 	{
-		buffer[i--] = (num % 8) + '0';
 		num /= 8;
+		counter++;
 	}
+	counter++;
+	array = malloc(counter * sizeof(int));
 
-	if (flags & F_HASH && init_num != 0)
-		buffer[i--] = '0';
-
-	i++;
-
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
-}
-
-/** PRINT UNSIGNED NUMBER IN HEXADECIMAL **/
-
-/**
- * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: argument list
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of char
- */
-
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	return (print_hexa(types, "0123456789abcdef", buffer,
-		flags, 'x', width, precision, size));
-}
-
-/** PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **/
-
-/**
- * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: arguments list
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of char
- */
-
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	return (print_hexa(types, "0123456789ABCDEF", buffer,
-		flags, 'X', width, precision, size));
-}
-
-/** PRINT HEXX NUM IN LOWER OR UPPER **/
-
-/**
- * print_hexa - Print hexadecimal number in lower or upper
- * @types: arguments list
- * @map_to: Array of value to map the number to
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @flag_ch: Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
- * @size: Size specification
- * Return: Number of char
- */
-
-int print_hexa(va_list types, char map_to[], char buffer[],
-	int flags, char flag_ch, int width, int precision, int size)
-{
-	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
-
-	UNUSED(width);
-
-	num = convert_size_unsgnd(num, size);
-
-	if (num == 0)
-		buffer[i--] = '0';
-
-	buffer[BUFF_SIZE - 1] = '\0';
-
-	while (num > 0)
+	for (i = 0; i < counter; i++)
 	{
-		buffer[i--] = map_to[num % 16];
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+#include "main.h"
+/**
+ * print_int - prints integer
+ * @args: argument to be printed
+ * Return: number of characters to be printed
+ */
+int print_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+/**
+ * print_dec - prints a decimal
+ * @args: argument to print
+ * Return: number of characters to be printed
+ */
+
+int print_dec(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int  i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+#include "main.h"
+
+/**
+ * print_hex_aux - prints a hexgecimal number
+ * @num: arguments
+ * Return: counter
+ */
+int print_hex_aux(unsigned long int num)
+{
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
 		num /= 16;
+		counter++;
 	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
 
-	if (flags & F_HASH && init_num != 0)
+	for (i = 0; i < counter; i++)
 	{
-		buffer[i--] = flag_ch;
-		buffer[i--] = '0';
+		array[i] = temp % 16;
+		temp /= 16;
 	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
 
-	i++;
+#include "main.h"
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+/**
+ * print_hex - prints a hexgecimal number
+ * @val: arguments
+ * Return: counter
+ */
+int print_hex(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
 }
