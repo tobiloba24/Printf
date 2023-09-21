b2b9475251a91ec2ba547cc45c4ce2b2a7dacca2
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-09-21 16:46:11 +0000

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * handle_print - To Print an argument based on it's type
+ * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
@@ -8,11 +8,11 @@
  * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
- * @size: specifies size
+ * @size: Size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
