66626b6f15c7be9d3c83bc5ee68478ec7c173841
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-09-21 14:34:51 +0000

    Create functions2.c

---
+++ functions2.c
@@ -0,0 +1,153 @@
+#include "main.h"
+/**
+ * printf_exclusive_string - print exclusuives string
+ * @val: argumen t
+ * Return: the length of a string
+ */
+
+int printf_exclusive_string(va_list val)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + printf_HEX_aux(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
+
+/**
+ * printf_bin - prints binary number
+ * @val: arguments
+ * Return: 1 (always)
+ */
+int printf_bin(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
+
+/**
+ * printf_HEX_aux - prints a hexgecimal number
+ * @num: number to be printed
+ * Return: counter
+ */
+int printf_HEX_aux(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * printf_HEX - prints a hexgecimal number
+ * @val: argument
+ * Return: counter
+ */
+int printf_HEX(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+/**
+ * printf_37 - prints the char 37
+ * Return: 1 (always)
+ */
+int printf_37(void)
+{
+	_putchar(37);
+	return (1);
+}
